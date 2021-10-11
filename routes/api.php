f35615a625ba0b5fa32873a9feba1b5c66fbcb81
Luciano Larama <chubaqac@gmail.com>
2021-10-11 06:08:14 -0300

    feat: add endpoints - auth and productos

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+
+// importamos los controladores
+use App\Http\Controllers\ProductosController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +23,39 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('logout', [AuthController::class, 'logout'])->middleware(['auth:api']);
+Route::get('user', [AuthController::class, 'user'])->middleware(['auth:api']);
+
+
+// Route::group([
+//     'middleware' => ['auth:api', 'rol']
+// ], function () {
+//     // Route::get('logout', [AuthController::class, 'logout']);
+//     // Route::get('user', [AuthController::class, 'user']);
+
+// });
+
+// Producto
+Route::get('/productos', [ProductosController::class, 'index']);
+Route::get('/productos/{id}', [ProductosController::class, 'show']);
+Route::post('/productos', [ProductosController::class, 'store']);
+Route::put('/productos/{id}', [ProductosController::class, 'update']);
+Route::delete('/productos/{id}', [ProductosController::class, 'destroy']);
+
+// Route::group([
+//     'prefix' => 'auth'
+// ], function () {
+//     Route::post('/register', [AuthController::class, 'register']);
+//     Route::post('/login', [AuthController::class, 'login']);
+
+//     Route::group([
+//         'middleware' => 'auth:api'
+//     ], function () {
+//         Route::get('logout', [AuthController::class, 'logout']);
+//         Route::get('user', [AuthController::class, 'user']);
+//     });
+// });
